0d48a3b742a79568c39358351500a8670a11dd0b
Talha Ansari <154464182+TalhaAnsari322@users.noreply.github.com>
2024-03-08 22:43:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,391 @@
+// =========CHAPTER 4=========
+// // -----------Question 1---------
+
+
+// var fullName = (" Talha Ansari ");
+// var myAge = (" 22 ");
+// var student = (" SMIT ");
+
+// alert( "Name:" + fullName + "Age:" + myAge + "Student of:" + student )
+
+// -----------Question 2--------
+
+//          LEGAL-VARIABLES IN JAVASCRIPT
+
+// var fullName =("fullName - is camel case variable thats JS recommended \n") ;
+// var _salary = ("_salary - Snake case is a legal and allowed in JS but not recommended\n") ;
+// var age = ("age - is a one word alphabets is legal in JS\n") ;
+// var xyz123 = ("xyz123 - Center & End number is legal variable but if variable start with a number is illegal\n") ;
+// var XYZ = ("XYZ - if my alphabates is capital is it legal in JS by google research") ;
+
+//          ILLEGAL-VARIABLES IN JAVASCRIPT
+
+// var 1stword = ("1stword - is illegal because its start with a number ")
+// var var = (" var - is illegal because they already reserved in JS")
+// var undefined = ("Undefined - reserved keyword in JS")
+// var my-variable = ("my-variable - is illegal its content hyphen word")
+// var my variable = ("my variable - illegal because its contain space")
+
+// -----------Question 3--------
+
+// var myHeading = "Rules for naming JS variables"
+// var myParagraph = "Variable names can only contain Alpha Letter Numeric Characters and Dollarsigns. <br> Variables must begin with a Dollarsign, underscores or letter. <br>  Variable names are case sensitive. <br>  Variable names should not be JS keywords."
+
+// document.write("<h1>" + myHeading + "</h1>")
+// document.write("<p>"  + myParagraph + "</p>")
+
+// -----------CHAPTER 5--------
+// -----------Question 1--------
+
+// var numPlus = (3+5)
+// var myWords = ("Sum of 3 and 5 is ")
+
+// document.write("<h1>" + myWords + numPlus + "</h1>")
+
+// -----------Question 2--------
+
+// var subs = (10 - 5)
+// var myWords = ("Subs of 10 and 5 is ")
+// document.write("<h1>" + myWords + subs + "</h1> ")
+
+// var multi = (10 * 5) 
+// var myWords = ("Multipilication of 10 and 5 is ")
+// document.write ("<h1>" + myWords + multi + "</h1>")
+
+// var div = (10 / 5)
+// var myWords =("Division of 10 and 5 is ")
+// document.write("<h1>" +  myWords + div + "</h1>")
+
+// var mod = (10 % 5)
+// var myWords =("Modulus of 10 and 5 is ")
+// document.write("<h1>" + myWords + mod + "</h1>")
+
+// -----------Question 3--------
+
+// var declare= 
+// document.write("Value after variable declaration is: " + declare + "<br>" )
+
+// var declare=(6);
+// document.write("Initial value: " + declare + "<br>")
+
+// declare++
+// document.write("Value after increment is: " + declare + "<br>")
+
+// declare+=(7);
+// document.write("value after addition is: " + declare + "<br>")
+
+// declare-=(10)
+// document.write("Value after Subtraction 10 the answer is: " + declare + "<br>")
+
+// declare+=(24)
+// document.write("Value after addition 24 the answer is: " + declare + "<br>")
+
+// declare-=12
+// document.write("Value after decrement is: " + declare + "<br>")
+
+// var remainder = 16 % 4
+// document.write("The remainder is : " + remainder)
+
+// -----------Question 4--------------
+
+// var ticket= 600 ;
+// var person= 5 ;
+// var multiTest= 600 * 5 ;
+// document.write("Total cost to buy " + person + " tickets to a movie is " + multiTest + "PKR")
+
+// -----------Question 5--------------
+// ---------FIRST WAY HOW I MAKE TABLE -----
+
+// var ptn1Heading = ("4 x 1 = ")
+// var multi1Ptn = 4 * 1 ;
+// var ptn2Heading = ("4 x 2 = ")
+// var multi2Ptn = 4 * 2 ;
+// var ptn3Heading = ("4 x 3 =")
+// var multi3Ptn = 4 * 3 ;
+// var ptn4Heading = ("4 x 4 = ")
+// var multi4Ptn = 4 * 4 ;
+// var ptn5Heading = ("4 x 5 = ")
+// var multi5Ptn = 4 * 5 ;
+// var ptn6Heading = ("4 x 6 = ")
+// var multi6Ptn = 4 * 6 ;
+// var ptn7Heading = ("4 x 7 = ")
+// var multi7Ptn = 4 * 7 ;
+// var ptn8Heading = ("4 x 8 = ")
+// var multi8Ptn = 4 * 8 ;
+// var ptn9Heading = ("4 x 9 = ")
+// var multi9Ptn = 4 * 9 ;
+// var ptn10Heading = ("4 x 10 = ")
+// var multi10Ptn = 4 * 10 ;
+
+// document.write("Table of 4 <br>" + ptn1Heading + multi1Ptn + "<br>" + ptn2Heading + multi2Ptn +  "<br>" + ptn3Heading + multi3Ptn + "<br>" + ptn4Heading + multi4Ptn + "<br>" + ptn5Heading + multi5Ptn + "<br>" + ptn6Heading + multi6Ptn + "<br>" + ptn7Heading + multi7Ptn + "<br>" + ptn8Heading + multi8Ptn + "<br>" + ptn9Heading + multi9Ptn + "<br>" + ptn10Heading + multi10Ptn)
+
+// ---------SECOND WAY HOW I MAKE TABLE -----
+
+// document.write("Table of 4 <br>")
+// var one = 4 * 1
+// document.write(" 4 x 1 = " + one)
+// var two = 4 * 2
+// document.write("<br> 4 x 2 = " + two)
+// var three = 4 * 3
+// document.write("<br> 4 x 3 = " + three)
+// var four = 4 * 4
+// document.write("<br> 4 x 4 = " + four)
+// var five = 4 * 5
+// document.write("<br> 4 x 5 = " + five)
+// var six = 4 * 6
+// document.write("<br> 4 x 6 = " + six)
+// var seven = 4 * 7
+// document.write("<br> 4 x 7 = " + seven)
+// var eight = 4 * 8
+// document.write("<br> 4 x 8 = " + eight)
+// var nine = 4 * 9
+// document.write("<br> 4 x 9 = " + nine)
+// var ten = 4 * 10
+// document.write("<br>4 x 10 = " + ten)
+
+// -----------Question 6--------------
+
+// --yeh mei nh samjha hun sai se iski phir practice krni hogi--
+
+// var fahrenheit = (25 * 9/5) + 32;
+// document.write("20°C is " + fahrenheit + "°F")
+// var celsius = (70-32)*5/9;
+// document.write("<br>70°F is " + celsius + "°C")
+
+// -----------Question 7--------------
+
+// var myHeading = ("Shopping Cart")
+// document.write("<h1>" + myHeading + "</h1>")
+
+// var priceOne = 650;
+// document.write("<br>Price of items 1 is " + priceOne + "<br>")
+
+// var orderNumOne = 3;
+// document.write("Quantity of item 1 is " + orderNumOne + "<br>")
+
+// var priceTwo = 100;
+// document.write("Price of item 2 is " + priceTwo + "<br>")
+
+// var orderNumTwo = 7;
+// document.write("Quantity of item 2 is " + orderNumTwo + "<br>")
+
+// var charges = 100;
+// document.write("Shipping charges is " + charges + "<br>")
+
+// var number1 = 650*3;
+// var number2 = 100*7;
+
+// var toTal = number1 + number2 + charges;
+// document.write("<br>Total cost of your order is " + toTal)
+
+// -----------Question 8--------------
+
+// var markSheets = ("Mark Sheets")
+// document.write("<h1>" + markSheets + "</h1>"    )
+
+// var totalMarks = 980;
+// document.write(" <br> Total Marks: " + totalMarks + "<br>")
+
+// var marksObtained = 804;
+// document.write("Marks Obtained: " + marksObtained + "<br>")
+
+// var final = 804/980*100;
+// document.write("Percentage: " +final+ "%")
+
+// -----------Question 9-------------
+
+// var currencyHeading =("Currency in pkr")
+// document.write("<h1>" + currencyHeading + "</h1>")
+
+// var usd = 10;
+// var riyal = 25;
+
+// var usdpkr = 104.80;
+// var riyalpkr = 28;
+
+// var usdToPkr = usd*usdpkr; 1048
+// var riyalToPkr = riyal*riyalpkr; 700
+ 
+
+// ---we should plus (1048 + 700) both number---//
+// var totalPkr = usdToPkr+riyalToPkr;
+
+// document.write("Total Currency in PKR: " + totalPkr)
+
+// -----------Question 10-------------
+
+// var metic = ((10+5)*10/2)
+// document.write("Result is : " + metic)
+
+// -----------Question 11-------------
+
+// var birthYear = 2002;
+// var currentYear = 2024;
+// var result = currentYear-birthYear;
+
+// document.write("They are either " + result + " years old")
+
+// ------------Question 12-----------
+
+// var headingOne = ("The Geometrizer")
+// document.write("<h1>" + headingOne + "</h1>");
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+
+
+// document.write("Radius of circle: " + radius)
+// document.write("<br>The circumference is: " + circumference)
+// document.write("<br>The area is: " + area)
+
+// ------------Question 13--------------
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>");
+
+// var bubble = "DingDong Splash Bubble";
+// document.write("Favourite snacks: " + bubble + "<br>");
+
+// var myAge = 21;
+// document.write("Current Age: " + myAge + "<br>");
+// var myMaximumAge = 70;
+// document.write("Estimited Maximum Age: " + myMaximumAge + "<br>");
+// var estimitedAmnt = 3;
+// document.write("Amount of snacks per day: " + estimitedAmnt + "<br>")
+
+// var limit = myMaximumAge - myAge;
+// var totalSnacks = (estimitedAmnt*365)*limit;
+
+// document.write("You will need " + totalSnacks + " " + bubble + " to last you until the ripe old age of " + myMaximumAge + ".");
+
+// ------------CHAPTER 6--------------
+// ------------Question 1--------------
+
+// document.write("<h1>Result: </h1>" + "<br>")
+
+// var number = 10; 
+// document.write("The value of a is: " + number + "<br>")
+// document.write("......................................."+ "<br>" + "<br>" + "<br>")
+
+// document.write("The value of ++a is: " + ++number + "<br>")
+// document.write("Now the value of a is: " + number + "<br>" + "<br>")
+
+// document.write("The value of a++ is: " + number + "<br>")
+// document.write("Now the value of a is: " + ++number + "<br>" + "<br>")
+
+// document.write("The value of --a is: " + --number + "<br>")
+// document.write("Now the value of a is: " + number + "<br>" + "<br>")
+
+// document.write("The value of a-- is: " + number-- + "<br>")
+// document.write("Now the value of a is: " + number + "<br>" + "<br>")
+
+// ------------Question 2--------------
+
+// var a=(2);
+// var b=(1);
+
+// (--a);
+// //now a is 1;
+
+// (--a - --b);
+// //now a 0 & and b is 0
+
+// (--a - --b)+(++b);now (-1 -1)+(1);
+// (--a - --b)+(++b) +(b--);now (-2 - -2)+(2)+(1)
+// // then
+// -2 + 2 + 2 + 2 =4
+
+// document.write("a will be= 2")
+// document.write("<br> b will be= 1")
+// document.write("<br> RESULT= 4")
+
+// ------------Question 2--------------
+//------ yeh sahi se samjhna hai ------------
+
+// var a = 2
+// var b = 1;
+
+// // Step 1: --a;
+// --a; // Ab a ki value 1 ho jayegi
+
+// // Step 2: --a - --b;
+// var step2 = --a - --b; // 1 - 0 = 1
+
+// // Step 3: --a - --b + ++b;
+// var step3 = --a - --b + ++b; // 1 - 0 + 1 = 2
+
+// // Step 4: --a - --b + ++b + b--;
+// var result = --a - --b + ++b + b--; // 1 - 0 + 1 - 1 = 1
+
+// document.write("<br> a: 1" ); 
+// document.write("<br> b: 0" ); 
+// document.write("<br> Result: 4 " ); 
+
+// ------------Question 3--------------
+
+// var myName= prompt("Enter your name:")
+// alert("hello, " + myName + "" + " Welcome to SMIT class.")
+
+// ------------Question 5--------------
+
+// nh kiya samjh nh aya
+
+
+// ------------Question 6--------------
+// ------------ BAS KARDIYA ---------
+
+// var english=("English");
+// var urdu=("Urdu");
+// var math=("Math");
+
+
+//  var totaleng=(100);
+//  var totalurd=(100);
+//  var totalmath=(100);
+
+//  var obteng=(54);
+//  var obturd=(78);
+//  var obtmath=(48);
+//  var per=((obteng+obtmath+obturd)*(100)/ (totaleng+totalmath+totalurd));
+
+//  document.write("<table >");
+
+//  document.write("<b>"+"Subject total marks Obtained marks Percentage"+  "</b>");
+//  document.write("<tr>");
+//  document.write("<td>"+"<b>"+"Subjects"+"</b>"+ "</td>");
+//  document.write("<td>"+"<b>"+"Total Marks"+"</b>"+ "</td>");
+//  document.write("<td>"+"<b>"+"Obtained Marks"+"</b>"+ "</td>");
+//  document.write("<td>"+"<b>"+"Percentage"+"</b>"+ "</td>");
+//  document.write("</tr>");
+
+
+//  document.write("</table");
+
+//  document.write("<table>");
+//  document.write("<tr>");
+//  document.write("<td>" +english  +"</td>");
+//  document.write("<td>" +totaleng  +"</td>");
+//  document.write("<td>" +obteng  +"</td>");
+//  document.write("<td>" +obteng*100/totaleng+"%"  +"</td>");
+//  document.write("</tr>");
+
+
+//  document.write("<tr>");
+//  document.write("<td>" +urdu  +"</td>");
+//  document.write("<td>" +totalurd  +"</td>");
+//  document.write("<td>" +obturd  +"</td>");
+//  document.write("<td>" +obturd*100/totalurd+"%"  +"</td>");
+//  document.write("</tr>");
+
+//  document.write("<tr>");
+//  document.write("<td>" +math  +"</td>");
+//  document.write("<td>" +totalmath  +"</td>");
+//  document.write("<td>" +obtmath  +"</td>");
+//  document.write("<td>" +obtmath*100/totalmath+"%"  +"</td>");
+//  document.write("</tr>");
+
+//  document.write("<tr>");
+//  document.write("<td>"+"</td>");
+//  document.write("<td>"+(totaleng+totalmath+totalurd) +"</td>");
+//  document.write("<td>"+(obteng+obturd+obtmath) +"</td>");
+//  document.write("<td>"+per+ "%"+"</td>");
